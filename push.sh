272c763e569ec291563bc8c389c1efd2ed9e2500
EC2 Default User <ec2-user@ip-10-0-101-175.ec2.internal>
2021-11-20 19:10:51 +0000

    adding push.sh

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "********************"
+echo "** Pushing image ***"
+echo "********************"
+
+echo "** Logging in ***"
+docker login -u eulukeborges -p $HUB_PASSWORD
+
+echo "*** Pushing image ***"
+docker push eulukeborges/vorx-jenkins:$BUILD_NUMBER
